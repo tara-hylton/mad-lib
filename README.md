bf5f40f72f2a6b8de086c6c95dc456d71cfe0587
tara-hylton <turrrleigh@gmail.com>
2025-02-25 17:17:49 -0500

    updated input and story for a better fit, added loops to verify alpha and numberic input, added loop for overall input, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # mad-lib
 Making a mad-lib  
 Initial story and input loaded, needs work  
+Fixed story and input  
+Added checks on input to verify alpha or numeric strings
+<!--Would like to add a couple of additional stories-->
--- main.py
+++ main.py
@@ -1,24 +1,54 @@
 #mad-lib
 
-print("Hi! Let's play a Mad Lib!\nPlease enter the requested words below to create a story.")
+def is_not_word(wrong):
+    return isinstance(wrong, str) and wrong.isnumeric()
 
-
-number1 = input("Please pick a number\n")
-person1 = input("Please pick a person or relative close to you\n")
-place1 = input("Please pick a place\n")
-adjective1 = input("Please pick an adjective\n")
-verb1 = input("Please pick a verb\n")
-noun1 = input("Please pick a noun\n")
-noun2 = input("Please pick a noun\n")
-adverb1 = input("Please pick an adverb\n")
-adverb2 = adverb1.capitalize()     #capitalize word
-animal1 = input("Please pick an animal\n")
-noun3 = input("Please pick a noun\n")
-place2 = input("Please pick a place\n")
-verb2 = input("Please pick a verb\n")  #add ed to verb
-verb3 = verb2 + 'ed'
-
-
-#Story
-print(f'When I was {number1} years old, my {person1} and I went to {place1} on a vacation. It was {adjective1} and we {verb1} to bring {noun1}. We both ended up with {noun2}. {adverb2} before we left, a baby {animal1} was found under the {noun3} and we had to chase it away so we could go to {place2}. It was a great trip and we wished we could have {verb3} forever!')
-
+while True:
+    answer = input(f'Would you like to do a Mad-lib? y/n\n').lower()
+    if answer in "y":
+        print("Please fill in the following requests:")
+        number1 = input("Please pick a number\n")
+        while number1.isalpha():
+            number1 = input("Please pick a number\n")
+        person1 = input("Please pick a celebrity\n")
+        while is_not_word(person1):
+            person1 = input("Please pick a celebrity\n")
+        place1 = input("Please pick a vacation spot\n")
+        while is_not_word(place1):
+            place1 = input("Please pick a vacation spot\n")
+        adjective1 = input("Please pick an adjective\n")
+        while is_not_word(adjective1):
+            adjective1 = input("Please pick an adjective\n")
+        verb1 = input("Please pick a verb\n")
+        while is_not_word(verb1):
+            verb1 = input("Please pick a verb\n")
+        noun1 = input("Please pick a noun\n")
+        while is_not_word(noun1):
+            noun1 = input("Please pick a noun\n")
+        noun4 = noun1 + 's'
+        noun2 = input("Please pick a noun\n")
+        while is_not_word(noun2):
+            noun2 = input("Please pick a noun\n")
+        adverb1 = input("Please pick an adverb\n")
+        while is_not_word(adverb1):
+            adverb1 = input("Please pick an adverb\n")
+        adverb2 = adverb1.capitalize()     #capitalize word
+        animal1 = input("Please pick an animal\n")
+        while is_not_word(animal1):
+            animal1 = input("Please pick an animal\n")
+        noun3 = input("Please pick a noun\n")
+        while is_not_word(noun3):
+            noun3 = input("Please pick a noun\n")
+        adjective2 = input("Please pick an adjective\n")
+        while is_not_word(adjective2):
+            adjective2 = input("Please pick an adjective\n")
+        verb2 = input("Please pick a verb\n")
+        while is_not_word(verb2):
+            verb2 = input("Please pick a verb\n")
+        #Story
+        print(f'When I was {number1} years old, {person1} and I went to {place1} on a vacation.\nIt was very {adjective1} which caused us to {verb1}.\nWe both ended up with {noun4}. :(\n{adverb2} before we left, a baby {animal1} was found under the {noun2} and we had to chase it away.\nAfterwards, we went to the {noun3}. It was a {adjective2} trip and we wished we could {verb2} forever!')
+    elif answer in "n":
+        print("Thank you for playing")
+        break
+    else:
+        print("I didn't understand your answer, try again")
